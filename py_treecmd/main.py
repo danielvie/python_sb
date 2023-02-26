660632bc364c7182c6f897f75a89ea43fff9f9e8
Daniel Vieira <danielvie@gmail.com>
2023-02-26 02:28:50 +0100

    py_treecmd: updates on pipe chars

--- py_treecmd/main.py
+++ py_treecmd/main.py
@@ -4,30 +4,7 @@ from pathlib import PurePath
 import select
 import sys
 import re
-
-class Node():
-    def __init__(self, name) -> None:
-        self.name  = name
-        self.child = []
-
-def DummyData():
-    params = [
-        'M       main.py', 
-        'M       Makefile', 
-        'A       lib/lib1.h', 
-        'A       lib/lib2.h', 
-        'A       lib/sub/l3.h', 
-        'A       lib/sub/l4.h', 
-        'A       lib/sub/l5.h', 
-        'A       lib/sub/subsub/l7.h', 
-        'A       lib/sub/subsub/l8.h', 
-        'A       lib/sub/subsub/l9.h', 
-        'A       lib/sub/subsub/l10.h', 
-        'A       src/bla.txt', 
-        'A       src/ble.txt', 
-    ]
-    
-    return params
+from helpers import getter
 
 
 if __name__ == "__main__":
@@ -35,7 +12,7 @@ if __name__ == "__main__":
     if select.select([sys.stdin, ], [], [], 0.0)[0]:
         params = sys.stdin.read().split('\n') 
     else:
-        params = DummyData()
+        params = getter.DummyParamsGit()
 
     
     patt = r'(?P<type>[A-Z])\s+' \
@@ -65,6 +42,7 @@ if __name__ == "__main__":
     char_end    = "└─"
 
     def show(N, tab = 0):
+        # instantiating persistent var in function
         if not hasattr(show, 'pipe'):
             show.pipe = True
             print(' ')
@@ -72,17 +50,15 @@ if __name__ == "__main__":
         numel_p = len(N)
 
         for i,ni in enumerate(N):
-            tab_ = '  '*tab
             nchildren = len(N[ni])
             
             # adding joints and pipes
+            tab_ = f'{char_pipe}  '*tab
+
             if i < numel_p - 1:
                 tab_ = f'{tab_}{char_middle}'
             else:
                 tab_ = f'{tab_}{char_end}'
-                
-            if tab == 0 and i == numel_p - 1:
-                show.pipe = False
                 
             if tab > 0 and show.pipe:
                 tab_ = f'{char_pipe}{tab_[1:]}'
