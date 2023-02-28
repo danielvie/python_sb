ddbf8ea13002b3386f2074efb540acac6f267445
Daniel Vieira <daniel.vieira@thermofisher.com>
2023-02-28 11:51:19 +0100

    adding arguments for `test`

--- py_treecmd/gtree.py
+++ py_treecmd/gtree.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
+import argparse
+from helpers import getter
 from pathlib import PurePath
-import sys
 import re
+import sys
 
-def main(params = None):
-    
-    if not params:
-        params = sys.stdin.read().split('\n') 
-    
+# defining joint chars
+char_pipe   = "│"
+char_middle = "├─"
+char_end    = "└─"
+
+def Buffer2Tree(params):
     patt = r'(?P<type>[A-Z])[\s\t]+' \
            r'(?P<param>\S+)'
     
@@ -32,54 +35,63 @@ def main(params = None):
                 
                 P.append(f"{p} [{value_r['type'][0]}]")
 
-    N = {}
+    tree = {}
     
     for pi in P:
         path = PurePath(pi)
-        N_   = N
+        tree_   = tree
         
         for part in path.parts:
-            if part not in N_:
-                N_[part] = {}
+            if part not in tree_:
+                tree_[part] = {}
 
-            N_ = N_[part]
-            
-    # defining joint chars
-    char_pipe   = "│"
-    char_middle = "├─"
-    char_end    = "└─"
-
-    def show(N, tab = 0):
-        numel_p = len(N)
-
-        for i,ni in enumerate(N):
-            nchildren = len(N[ni])
-            
-            # adding joints and pipes
-            tab_ = f'{char_pipe}  '*tab
-
-            if i < numel_p - 1:
-                tab_ = f'{tab_}{char_middle}'
-            else:
-                tab_ = f'{tab_}{char_end}'
-                
-            # writing message
-            msg = f'{tab_}{ni}'
-            print(msg)
-
-            # if there is no children, continue
-            if nchildren == 0:
-                continue
-            
-            show(N[ni], tab + 1)
+            tree_ = tree_[part]
     
-    show(N)
+    return tree
+
+def show(N, tab = 0):
+    numel_p = len(N)
+
+    for i,ni in enumerate(N):
+        nchildren = len(N[ni])
+        
+        # adding joints and pipes
+        tab_ = f'{char_pipe}  '*tab
+
+        if i < numel_p - 1:
+            tab_ = f'{tab_}{char_middle}'
+        else:
+            tab_ = f'{tab_}{char_end}'
+            
+        # writing message
+        msg = f'{tab_}{ni}'
+        print(msg)
+
+        # if there is no children, continue
+        if nchildren == 0:
+            continue
+        
+        show(N[ni], tab + 1)
+
+def main(params = None):
+    
+    if not params:
+        params = sys.stdin.read().split('\n') 
+    
+    tree = Buffer2Tree(params)
+    show(tree)
 
 if __name__ == "__main__":
-    
-    from helpers import getter
 
-    main()
-    # main(getter.DummyDataFromFile('ref.txt'))
+    parser = argparse.ArgumentParser(description='Shows the output of the git `compare` as a tree')
+    parser.add_argument('--test', action="store_true")
+    parser.add_argument('-f', '--file', type=str, help="read from 'file' with git output")
 
+    args = parser.parse_args()
     
+    if args.test:
+        main(getter.DummyDataFromFile('ref.txt'))
+    elif args.file:
+        main(getter.DummyDataFromFile(args.file))
+    else:
+        main()
